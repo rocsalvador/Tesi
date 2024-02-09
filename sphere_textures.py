Fix normal calculation

sphere_textures.py
@@ -1,10 +1,16 @@
 import numpy as np
 import random
 from PIL import Image
+import sys
 
-n = 1024
-r = 40
-threshold_prob = 0.0001
+if len(sys.argv) < 4:
+    print("Usage:")
+    print("python3 sphere_textures.py n r p")
+    exit(1)
+
+n = int(sys.argv[1])
+r = int(sys.argv[2])
+threshold_prob = float(sys.argv[3])
 
 norm_map = np.zeros((n, n, 3), 'uint8')
 color_map = np.zeros((n, n, 3), 'uint8')
@@ -13,16 +19,17 @@ roughness_map =  np.zeros((n, n), 'uint8')
 def draw_sphere_textures(x, y, radius):
     color = [random.uniform(0, 128), random.uniform(192, 255), random.uniform(0, 128)]
     roughness = random.uniform(0, 255)
-    for i in range(x - radius, x + radius):
-        for j in range(y - radius, y + radius):
-            if (i >= 0 and i < n and j >= 0 and j < n and pow((pow((i - x), 2) + pow((j - y), 2)), 0.5) <= radius):
-                norm_x = abs(i - x)
-                norm_y = abs(j - y)
-                xy_length = pow(pow(norm_x, 2) + pow(norm_y, 2), 0.5)
-                norm_z = pow(pow(radius, 2) - pow(xy_length, 2), 0.5)
-                unit_factor =  (norm_x + norm_y + norm_z)
-                norm_map[i, j] = [norm_x / unit_factor * 255, norm_y / unit_factor * 255, norm_z / unit_factor * 255]
-
+    
+    for i in range(max(0, x - radius + 1), min(x + radius, n)):
+        for j in range(max(0, y - radius + 1), min(y + radius, n)):
+            distance = pow((pow((i - x), 2) + pow((j - y), 2)), 0.5)
+            if distance <= radius:
+                norm_x = i - x
+                norm_y = j - y
+                norm_z = pow(pow(radius, 2) - pow(distance, 2), 0.5)
+                unit_factor =  (abs(norm_x) + abs(norm_y) + norm_z)
+                norm_map[i, j] = [127 + (norm_x / unit_factor * 128), 127 + (norm_y / unit_factor * 128), 127 + (norm_z / unit_factor * 128)]
+                
                 color_map[i, j] = color
 
                 roughness_map[i, j] = roughness